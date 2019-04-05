43ec6a11278cc56a548806667d4b72d6c84961c9
RichyYhcir <47454423+RichyYhcir@users.noreply.github.com>
2019-04-05 16:50:06 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'FwEeFYUcgzzYFbrPeel7H/rJfzfgCEdjHD9U7UTQ4koa5K5MFE/IMNBxuGgcme+bpjVBPgMxLGRpZbINWuJinRJ3gH66as5n96KixvL2/BFSXFCKlnDgggWZpjQ2YsLfKHLusYlo6uhgRTnjCYi02gdB04t89/1O/w1cDnyilFU=';
+$access_token = '/1fyXsNQtqg0KIa9fFA7drEkgisYBI9fvDpsAQOgEQRY+/m7+vVUA57JP7sG4tq0kh5LngFuo2ZfpgYMZTVyNF/dtbX2zl37WSc45FJjsBRQiDl7XITQPNfROVX0PO63KkVJudoJtT14xlSRYokPDQdB04t89/1O/w1cDnyilFU=';
 $url = 'https://api.line.me/v1/oauth/verify';
 $headers = array('Authorization: Bearer ' . $access_token);
 
