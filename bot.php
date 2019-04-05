971f01e399a6575b0ed5a603eb24486786d68c91
RichyYhcir <47454423+RichyYhcir@users.noreply.github.com>
2019-04-05 17:39:12 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -9,8 +9,8 @@ $content = file_get_contents('php://input');
 $events = json_decode($content, true);
 
 // Validate parsed JSON data
-if (!is_null($events['ESP'])) {
-   send_LINE($events['ESP']);
+if (!is_null($events['banana'])) {
+   send_LINE($events['banana']);
    echo "OK";
 }
 
@@ -25,7 +25,7 @@ if (!is_null($events['events'])) {
 	   // Get replyToken
 	   $replyToken = $event['replyToken'];
   	   // Build message to reply back
-           $Topic = "ESP32" ;
+           $Topic = "banana" ;
 	   getMqttfromlineMsg($Topic,$text);
 	}
     }
